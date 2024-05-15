83ff9f183f5dbf9dea445b43ff94ad4f26086468
zainraza2000 <zainraza20000@gmail.com>
2024-05-15 19:51:03 +0500

    fix

--- server.js
+++ server.js
@@ -1,35 +1,48 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const jsonData = require('./data.json'); // Assuming your JSON data is in data.json file
+const cors = require("cors");
+const professionsData = require("./professions.json");
+const jsonData = require("./data.json"); // Assuming your JSON data is in data.json file
+
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Allow requests from this origin
+  })
+);
 
 // API endpoint to search based on type and query string
-app.get('/search', (req, res) => {
-    const { type, query } = req.query;
+app.get("/search", (req, res) => {
+  const { type, query } = req.query;
 
-    if (!type || !query) {
-        return res.status(400).json({ error: "Type and query parameters are required." });
-    }
+  if (!type || !query) {
+    return res
+      .status(400)
+      .json({ error: "Type and query parameters are required." });
+  }
 
-    // Filtering the jsonData based on type and query
-    const filteredData = jsonData.filter(item => item.type === type && new RegExp(query, 'i').test(item.name));
+  // Filtering the jsonData based on type and query
+  const filteredData = jsonData.filter(
+    (item) => item.type === type && new RegExp(query, "i").test(item.name)
+  );
 
-    res.json(filteredData);
+  res.json(filteredData);
 });
 
-app.get('/search-professions', (req, res) => {
-    const { query } = req.query;
+app.get("/search-professions", (req, res) => {
+  const { query } = req.query;
 
-    if (!query) {
-        return res.status(400).json({ error: "Query parameter is required." });
-    }
+  if (!query) {
+    return res.status(400).json({ error: "Query parameter is required." });
+  }
+  // Filtering the professionsData based on the query
+  const filteredProfessions = professionsData.filter((profession) =>
+    profession.name.toLowerCase().includes(query.toLowerCase())
+  );
 
-    // Filtering the professionsData based on the query
-    const filteredProfessions = professionsData.filter(profession => profession.name.toLowerCase().includes(query.toLowerCase()));
-
-    res.json(filteredProfessions);
+  res.json(filteredProfessions);
 });
 
 const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
